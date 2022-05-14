c45850caf467475f3de2b84d2ba23cb22e3976d1
Alireza Taghavi <taghavizadeh.alireza@gmail.com>
2022-05-14 21:09:26 +0430

    uploading html and css

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+//DOM
+list=document.getElementById("list");
+main = document.getElementsByTagName("main");
+
+//Fetch
+class warn{
+    constructor(x){
+        this.name = x;
+    }
+
+    showAlert= async()=>{
+        await alert(this.name)
+    }
+}
+class red {
+    getPrompt=()=>{
+        return prompt("give sting")
+    }
+}
