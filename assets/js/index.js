72f5e618035a3cc03d5e9eda0371c2ecc2499be7
Alireza Taghavi <taghavizadeh.alireza@gmail.com>
2022-05-16 02:42:40 +0430

    added modal and rewrote some flexes with grids

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+const main = document.getElementsByTagName("main");
+const list = document.getElementById("list");
+const disInput = document.getElementById("dis-code");
+const disSubmit = document.getElementById("submit-dis");
+const price = document.getElementById("price");
+const taxes = document.getElementById("taxes");
+const discount = document.getElementById("discount");
+const total = document.getElementById("total")
+const submit = document.getElementById("submit");
+
