4650b972f1bf0be514a6980c9ef2761a2a9c158b
Alireza Taghavi <taghavizadeh.alireza@gmail.com>
2022-05-16 11:12:43 +0430

    added fetch + loaders

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,6 @@
+const main = document.querySelector("main");
+
+
 let menu, res;
 
 class getData {
@@ -11,14 +14,21 @@ class getData {
     async loadMenu() {
         this.res = await fetch(this.menuConst);
         this.menu = await this.res.json();
-        menu = this.menu;
-        console.log(menu);
+        await this.menuSetter();
     }
 
     async loadOrders() {
         this.res2 = await fetch(this.ordersConst);
         this.orders = await this.res2.json();
         console.log(this.orders);
+    }
+
+    menuSetter() {
+        menu = this.menu;
+        main.innerHTML = "";
+        this.menu.forEach(item =>
+            main.innerHTML += `<div class="item d-flex jst-cnt-btw"><div class="left-item d-flex flex-column align-itms-strt"><div class="rate-holder"><span class="rate">${item.stars}</span><i class="fa fa-star" aria-hidden="true"></i></div><div class="info-box d-flex flex-column align-itms-strt gap0-5"><div class="txt-lft">${item.foodName}</div></div></div><div class="right-item  d-flex flex-column align-itms-cnt"><div class="item-pic d-flex flex-column"><div class="pic-holder"><img src="${item.imageLink}" alt="Spicy Rote"></div><div class="item-amount d-flex flex-column"><i class="fa fa-certificate" aria-hidden="true"></i><span class="amount">4</span></div></div><span class=" each-price">${item.price}</span></div></div>`
+        )
     }
 }
 
