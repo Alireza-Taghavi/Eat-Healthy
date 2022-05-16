162835106926458ff1368756f68ea49f6174ff0b
Alireza Taghavi <taghavizadeh.alireza@gmail.com>
2022-05-16 12:17:33 +0430

    responsive + some adjustments

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,13 @@
 const main = document.querySelector("main");
+const list = document.getElementById("list");
 
-
-let menu, res;
+let menu;
 
 class getData {
     constructor() {
         this.menuConst = "http://localhost:3000/menu";
         this.ordersConst = "http://localhost:3000/orders";
+        this.reset();
         this.loadMenu();
         this.loadOrders();
     }
@@ -22,14 +23,17 @@ class getData {
         this.orders = await this.res2.json();
         console.log(this.orders);
     }
-
+    reset(){
+        main.innerHTML = "";
+        list.innerHTML = "";
+    }
     menuSetter() {
         menu = this.menu;
-        main.innerHTML = "";
         this.menu.forEach(item =>
-            main.innerHTML += `<div class="item d-flex jst-cnt-btw"><div class="left-item d-flex flex-column align-itms-strt"><div class="rate-holder"><span class="rate">${item.stars}</span><i class="fa fa-star" aria-hidden="true"></i></div><div class="info-box d-flex flex-column align-itms-strt gap0-5"><div class="txt-lft">${item.foodName}</div></div></div><div class="right-item  d-flex flex-column align-itms-cnt"><div class="item-pic d-flex flex-column"><div class="pic-holder"><img src="${item.imageLink}" alt="Spicy Rote"></div><div class="item-amount d-flex flex-column"><i class="fa fa-certificate" aria-hidden="true"></i><span class="amount">4</span></div></div><span class=" each-price">${item.price}</span></div></div>`
+            main.innerHTML += `<div class="item d-flex jst-cnt-btw"><div class="left-item d-flex flex-column align-itms-strt"><div class="rate-holder"><span class="rate">${item.stars}</span><i class="fa fa-star" aria-hidden="true"></i></div><div class="info-box d-flex flex-column align-itms-strt gap0-5"><div class="txt-lft">${item.foodName}</div></div></div><div class="right-item  d-flex flex-column align-itms-cnt"><div class="item-pic d-flex flex-column"><div class="pic-holder"><img src="${item.imageLink}" alt="Spicy Rote"></div><div class="item-amount d-flex flex-column"><i class="fa fa-certificate" aria-hidden="true"></i><span class="amount">0</span></div></div><span class=" each-price">${item.price}</span></div></div>`
         )
     }
+
 }
 
-const head = new getData();
+new getData();
