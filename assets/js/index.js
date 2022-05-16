d28710725c899ada81f9dad7836568ce4f858d13
Alireza Taghavi <taghavizadeh.alireza@gmail.com>
2022-05-16 13:06:32 +0430

    starting modal

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,11 @@
+
 const main = document.querySelector("main");
 const list = document.getElementById("list");
+const modalBG = document.querySelector(".modal-bg");
+const modal = modalBG.querySelector(".modal")
 
 let menu;
+
 
 class getData {
     constructor() {
@@ -30,10 +34,23 @@ class getData {
     menuSetter() {
         menu = this.menu;
         this.menu.forEach(item =>
-            main.innerHTML += `<div class="item d-flex jst-cnt-btw"><div class="left-item d-flex flex-column align-itms-strt"><div class="rate-holder"><span class="rate">${item.stars}</span><i class="fa fa-star" aria-hidden="true"></i></div><div class="info-box d-flex flex-column align-itms-strt gap0-5"><div class="txt-lft">${item.foodName}</div></div></div><div class="right-item  d-flex flex-column align-itms-cnt"><div class="item-pic d-flex flex-column"><div class="pic-holder"><img src="${item.imageLink}" alt="Spicy Rote"></div><div class="item-amount d-flex flex-column"><i class="fa fa-certificate" aria-hidden="true"></i><span class="amount">0</span></div></div><span class=" each-price">${item.price}</span></div></div>`
+            main.innerHTML += `<div id="${item.id}" onclick="og.openModal(event)" class="item d-flex jst-cnt-btw"><div class="left-item d-flex flex-column align-itms-strt"><div class="rate-holder"><span class="rate">${item.stars}</span><i class="fa fa-star" aria-hidden="true"></i></div><div class="info-box d-flex flex-column align-itms-strt gap0-5"><div class="txt-lft">${item.foodName}</div></div></div><div class="right-item  d-flex flex-column align-itms-cnt"><div class="item-pic d-flex flex-column"><div class="pic-holder"><img src="${item.imageLink}" alt="Spicy Rote"></div><div class="item-amount d-flex flex-column"><i class="fa fa-certificate" aria-hidden="true"></i><span class="amount">0</span></div></div><span class=" each-price">${item.price}</span></div></div>`
         )
     }
+    closeModal(){
+        modalBG.style.display = "none";
+    }
+    openModal(e){
+        console.log(e.target);
+        modalBG.style.display = "flex";
 
+    }
 }
 
-new getData();
+
+const og = new getData();
+
+modalBG.addEventListener("click", og.closeModal, false);
+modal.addEventListener("click", (event)=>{event.stopPropagation();},false);
+document.getElementById("modal-close").addEventListener("click",  og.closeModal);
+
